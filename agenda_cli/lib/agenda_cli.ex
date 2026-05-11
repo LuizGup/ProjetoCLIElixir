9b31500b677ce1735c13bad0d4470bcb20a8b8ff
Luiz Carlos Filho <lcarlos.mln@gmail.com>
2026-05-11 00:41:45 -0300

    Initialize project structure with Mix, including formatter, gitignore, and basic application setup

---
+++ agenda_cli/lib/agenda_cli.ex
@@ -0,0 +1,18 @@
+defmodule AgendaCli do
+  @moduledoc """
+  Documentation for `AgendaCli`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AgendaCli.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
